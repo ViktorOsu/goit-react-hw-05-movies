9c432934bdc8bcc974f267eb407b02113d774cdf
ViktorOsu <123021878+ViktorOsu@users.noreply.github.com>
2023-05-10 22:33:52 +0300

    ol

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 export const NotFound = () => {
   return (
     <>
-      <p>Oops...</p>
+      <p>Ooops...</p>
       <p> Page not found...</p>
       <NavLink to="/"> Back to Home Page </NavLink>
     </>
